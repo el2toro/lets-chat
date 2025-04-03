080cb88b491698406899fe13c355e6dc6eded55a
el2toro <vasea.badragan@gmail.com>
2025-04-03 18:40:17 +0200

    add login api

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,16 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserModel } from '../features/models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-constructor() { }
+constructor(private httpClient: HttpClient) { }
+private baseUrl = 'https://localhost:7015';
+
   isLoggedIn(): boolean {
     return !!localStorage.getItem('user'); // Example logic (use JWT, API calls, etc.)
   }
 
   logout(){
-    localStorage.setItem('user', '');
+    localStorage.removeItem('user');
+  }
+
+  login(userName: string, password: string): Observable<any>{
+   return this.httpClient.post<any>(this.baseUrl + '/login', {userName: userName, password: password})
+  }
+
+  getLoggedInUser(): UserModel{
+    return JSON.parse(localStorage.getItem('user') as string) as UserModel;
   }
 }
