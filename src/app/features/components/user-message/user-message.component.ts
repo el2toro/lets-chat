b6fbe41e18ac40b8e2070fc569ac68a55cb8ac89
el2toro <vasea.badragan@gmail.com>
2025-04-03 21:19:35 +0200

    build user message component

--- src/app/features/components/user-message/user-message.component.ts
+++ src/app/features/components/user-message/user-message.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MessageModel } from '../../models/message.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-message',
   templateUrl: './user-message.component.html',
-  styleUrls: ['./user-message.component.scss']
+  styleUrls: ['./user-message.component.scss'],
+  imports: [CommonModule]
 })
 export class UserMessageComponent implements OnInit {
+ @Input() messages = <MessageModel[]>[];
+ @Input() userFullName = '';
 
   constructor() { }
 
