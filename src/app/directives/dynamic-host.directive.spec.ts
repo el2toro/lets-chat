1d5d51982910f05b4aeb685cd6bf32fab2c4ec43
el2toro <vasea.badragan@gmail.com>
2025-04-03 21:18:41 +0200

    create dynamic directive

---
+++ src/app/directives/dynamic-host.directive.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { DynamicHostDirective } from './dynamic-host.directive';
+
+describe('Directive: DynamicHost', () => {
+  it('should create an instance', () => {
+    const directive = new DynamicHostDirective();
+    expect(directive).toBeTruthy();
+  });
+});
